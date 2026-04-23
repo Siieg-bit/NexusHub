58cc8eaaef50d1f1985adb02e3b25dbef9334e87
Crystopher Felisberto <crystopherfelisberto@hotmail.com>
2026-04-23 17:19:11 -0400

    fix: adicionar import supabase_flutter no cosmetics_provider

--- frontend/lib/core/providers/cosmetics_provider.dart
+++ frontend/lib/core/providers/cosmetics_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import '../services/supabase_service.dart';
 import '../services/realtime_service.dart';
 
